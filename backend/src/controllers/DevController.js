22454cec87c6aa248ebb78a765743fbabf98657e
liammaricato <liammaricato@gmail.com>
2020-01-18 11:00:48 -0300

    :bug:

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   async store (request, response) {
-    const { github_username, techs, latitude, longitude  } = request.body;
+    const { github_username, techs, latitude, longitude } = request.body;
     
     let dev = await Dev.findOne({ github_username });
 
