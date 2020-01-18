d0bdd117e417d6f941dc4f074adb87e143e27965
liammaricato <liammaricato@gmail.com>
2020-01-18 10:43:19 -0300

    :poop: this frontend is bad, just bad

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://liam:omnistack@omnistack10-bg8zh.mongodb.net/wee
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
